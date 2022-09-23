eeb5cc66b7ed562ea4af85b602fe2bbdd8e808d2
lfelipe.afreitas <lfelipe.afreitas@gmail.com>
2022-09-23 15:31:35 -0300

    projeto criado

---
+++ src/main/java/com/luisFelipeAF/cursoOOP/CursoOopApplication.java
@@ -0,0 +1,13 @@
+package com.luisFelipeAF.cursoOOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoOopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisFelipeAF/cursoOOP/CursoOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisFelipeAF.cursoOOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
